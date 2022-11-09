f3f54cb24f72ae63485c9349ec2d20d5c975e340
seliverstov <seliverstov@SELIVERSTOV1154>
2022-11-09 17:13:08 +0300

    Task 2 released

--- Dishes.py
+++ Dishes.py
@@ -1,3 +1,4 @@
+
 recipes = {}
 with open('recipes.txt', 'r', encoding='utf8') as f_recipes:
     for dish in f_recipes:
@@ -9,4 +10,20 @@ with open('recipes.txt', 'r', encoding='utf8') as f_recipes:
         recipes[dish[:-1]] = ing_list
         f_recipes.readline()
 
-print(recipes)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in recipes.keys():
+            for ing in recipes[dish]:
+                ing_name, ing_quantity, ing_measure = ing['ingredient_name'], ing['quantity'], ing['measure']
+                if ing_name in shop_list.keys():
+                    shop_list[ing_name]['quantity'] += ing_quantity * person_count
+                else:
+                    shop_list[ing_name] = {'measure': ing_measure, 'quantity': ing_quantity * person_count}
+        else:
+            return 'Рецепт как минимум одного блюда не найден'
+    return shop_list
+
+
+print(get_shop_list_by_dishes(['Утка по-пекински', 'Чай'], 6))
